0c044ceb4f6a0201f5095537d2cde2a578deb9f4
Abhishek Krishnamurthy <abhishek.k@accionlabs.com>
2023-05-19 12:26:25 +0530

    added table with pagination

--- src/App.js
+++ src/App.js
@@ -88,4 +88,110 @@ const NavigationMenu = () => {
   );
 };
 
-export default NavigationMenu;
+const Table = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
+
+  const data = [
+    { name: "Rahul", age: 25, email: "abhishek@gmail.com" },
+    { name: "Amit", age: 30, email: "deepak@gmail.com" },
+    { name: "Sandeep", age: 35, email: "sathish@gmail.com" },
+    { name: "Anjali", age: 28, email: "anjali@gmail.com" },
+    { name: "Manish", age: 32, email: "manish@gmail.com" },
+    { name: "Priya", age: 29, email: "priya@gmail.com" },
+    { name: "Saurabh", age: 33, email: "saurabh@gmail.com" },
+    { name: "Pooja", age: 27, email: "pooja@gmail.com" },
+    { name: "Vivek", age: 31, email: "vivek@gmail.com" },
+    { name: "Neha", age: 26, email: "neha@gmail.com" },
+    { name: "Rajesh", age: 34, email: "rajesh@gmail.com" },
+    { name: "Aarti", age: 28, email: "aarti@gmail.com" },
+    { name: "Rohit", age: 32, email: "rohit@gmail.com" },
+    { name: "Shweta", age: 29, email: "shweta@gmail.com" },
+    { name: "Avinash", age: 33, email: "avinash@gmail.com" },
+    { name: "Smita", age: 27, email: "smita@gmail.com" },
+    { name: "Alok", age: 31, email: "alok@gmail.com" },
+    { name: "Kavita", age: 26, email: "kavita@gmail.com" },
+    { name: "Amar", age: 34, email: "amar@gmail.com" },
+    { name: "Sneha", age: 28, email: "sneha@gmail.com" },
+    { name: "Alok", age: 32, email: "alok2@gmail.com" },
+    { name: "Kiran", age: 29, email: "kiran@gmail.com" },
+    { name: "Ajay", age: 33, email: "ajay@gmail.com" },
+    { name: "Swati", age: 27, email: "swati@gmail.com" },
+    { name: "Ashish", age: 31, email: "ashish@gmail.com" },
+    { name: "Shilpa", age: 26, email: "shilpa@gmail.com" },
+    { name: "Sanjay", age: 34, email: "sanjay@gmail.com" },
+    { name: "Kajal", age: 28, email: "kajal@gmail.com" },
+    { name: "Avinash", age: 32, email: "avinash2@gmail.com" },
+    { name: "Meena", age: 29, email: "meena@gmail.com" },
+    { name: "Arun", age: 33, email: "arun@gmail.com" },
+  ];
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div>
+      <table className="main-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+              <td>{item.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Pagination
+        itemsPerPage={itemsPerPage}
+        totalItems={data.length}
+        currentPage={currentPage}
+        paginate={paginate}
+      />
+    </div>
+  );
+};
+
+const Pagination = ({ itemsPerPage, totalItems, currentPage, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <ul className="pagination">
+      {pageNumbers.map((number) => (
+        <li
+          key={number}
+          className={number === currentPage ? "active" : ""}
+          onClick={() => paginate(number)}
+        >
+          {number}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <NavigationMenu />
+      <Table />
+    </div>
+  );
+}
